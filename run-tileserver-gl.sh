2042c3a0c559f0cf334564dc700817b837d06513
StefanHaselwimmer <shaselwimmer@cantab.net>
2025-06-02 14:59:15 +0100

    Set tileserver-gl to correct domain

--- run-tileserver-gl.sh
+++ run-tileserver-gl.sh
@@ -3,12 +3,6 @@
 export LD_LIBRARY_PATH=/opt/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=/opt/lib/pkgconfig:$PKG_CONFIG_PATH
 export EXTERNAL_IP=$(curl ipinfo.io/ip)
-export PUBLIC_URL=http://${EXTERNAL_IP}/tiles/
-
-if [ -f "/usr/src/openwindenergy/DOMAINACTIVE" ]; then
-    . /usr/src/openwindenergy/DOMAINACTIVE
-    export PUBLIC_URL=https://${DOMAIN}/tiles/
-fi
-
+export PUBLIC_URL=https://tiles.votewind.org
 
 xvfb-run --server-args="-screen 0 1024x768x24" tileserver-gl -p 8080 --public_url ${PUBLIC_URL} --config build-cli/tileserver/config.json
